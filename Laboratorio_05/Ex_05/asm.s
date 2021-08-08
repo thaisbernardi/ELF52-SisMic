73c219a3114ba62996f75e6d444eb5f287220262
thaisbernardi <39557310+thaisbernardi@users.noreply.github.com>
2021-08-08 00:15:08 -0300

    Add files via upload

---
+++ Laboratorio_05/Ex_05/asm.s
@@ -0,0 +1,228 @@
+        PUBLIC  __iar_program_start
+        EXTERN  __vector_table
+
+        SECTION .text:CODE:REORDER(2)
+        
+        ;; Keep vector table even if it's not referenced
+        REQUIRE __vector_table
+        
+        THUMB
+
+; System Control definitions
+SYSCTL_BASE             EQU     0x400FE000
+SYSCTL_RCGCGPIO         EQU     0x0608
+SYSCTL_PRGPIO		EQU     0x0A08
+SYSCTL_RCGCUART         EQU     0x0618
+SYSCTL_PRUART           EQU     0x0A18
+; System Control bit definitions
+PORTA_BIT               EQU     000000000000001b ; bit  0 = Port A
+PORTF_BIT               EQU     000000000100000b ; bit  5 = Port F
+PORTJ_BIT               EQU     000000100000000b ; bit  8 = Port J
+PORTN_BIT               EQU     001000000000000b ; bit 12 = Port N
+UART0_BIT               EQU     00000001b        ; bit  0 = UART 0
+
+; NVIC definitions
+NVIC_BASE               EQU     0xE000E000
+NVIC_EN1                EQU     0x0104
+VIC_DIS1                EQU     0x0184
+NVIC_PEND1              EQU     0x0204
+NVIC_UNPEND1            EQU     0x0284
+NVIC_ACTIVE1            EQU     0x0304
+NVIC_PRI12              EQU     0x0430
+
+; GPIO Port definitions
+GPIO_PORTA_BASE         EQU     0x40058000
+GPIO_PORTF_BASE    	EQU     0x4005D000
+GPIO_PORTJ_BASE    	EQU     0x40060000
+GPIO_PORTN_BASE    	EQU     0x40064000
+GPIO_DIR                EQU     0x0400
+GPIO_IS                 EQU     0x0404
+GPIO_IBE                EQU     0x0408
+GPIO_IEV                EQU     0x040C
+GPIO_IM                 EQU     0x0410
+GPIO_RIS                EQU     0x0414
+GPIO_MIS                EQU     0x0418
+GPIO_ICR                EQU     0x041C
+GPIO_AFSEL              EQU     0x0420
+GPIO_PUR                EQU     0x0510
+GPIO_DEN                EQU     0x051C
+GPIO_PCTL               EQU     0x052C
+
+; UART definitions
+UART_PORT0_BASE         EQU     0x4000C000
+UART_FR                 EQU     0x0018
+UART_IBRD               EQU     0x0024
+UART_FBRD               EQU     0x0028
+UART_LCRH               EQU     0x002C
+UART_CTL                EQU     0x0030
+UART_CC                 EQU     0x0FC8
+;UART bit definitions
+TXFE_BIT                EQU     10000000b ; TX FIFO full
+RXFF_BIT                EQU     01000000b ; RX FIFO empty
+BUSY_BIT                EQU     00001000b ; Busy
+
+    
+
+    
+; PROGRAMA PRINCIPAL
+
+__iar_program_start
+        
+main:   MOV R2, #(UART0_BIT)
+	BL UART_enable ; habilita clock ao port 0 de UART
+
+        MOV R2, #(PORTA_BIT)
+	BL GPIO_enable ; habilita clock ao port A de GPIO
+        
+	LDR R0, =GPIO_PORTA_BASE
+        MOV R1, #00000011b ; bits 0 e 1 como especiais
+        BL GPIO_special
+
+	MOV R1, #0xFF ; máscara das funções especiais no port A (bits 1 e 0)
+        MOV R2, #0x11  ; funções especiais RX e TX no port A (UART)
+        BL GPIO_select
+
+	LDR R0, =UART_PORT0_BASE
+        BL UART_config ; configura periférico UART0
+        
+        ; recepção e envio de dados pela UART utilizando sondagem (polling)
+        ; resulta em um "eco": dados recebidos são retransmitidos pela UART
+
+       
+loop:
+wrx:    LDR R2, [R0, #UART_FR] ; status da UART
+        TST R2, #RXFF_BIT ; receptor cheio?
+        BEQ wrx
+        LDR R1, [R0] ; lê do registrador de dados da UART0 (recebe)
+        BL UART_reply
+        
+        B loop
+
+
+; SUB-ROTINAS
+
+;----------
+
+;;UART_Reply: itera pelos elements de REPLY pra enviar caracter por caractere na UART
+;R0 endereço, R1 valor "trigger", R2 constante de resposta, R3 tamanho/iterador
+
+UART_reply: 
+      
+       CMP R1, #0x0D ; R1 == /r
+       BNE reply_end 
+      
+       LDR R2, =REPLY ;String constant
+       MOVS R3, #27   ;string constant size
+      
+tx_loop      ;loop para terminar o envio atual
+       LDR R4, [R0, #UART_FR] ; status da UART
+       TST R4, #TXFE_BIT ; transmissor vazio?
+       BEQ tx_loop
+       
+       LDR R5, [R2]
+       STR R5, [R0]
+      
+       ADD R2, R2, #1
+       SUB R3, R3, #1
+      
+       CMP R3, #0
+       BNE tx_loop
+            
+reply_end
+      BX LR
+
+; UART_enable: habilita clock para as UARTs selecionadas em R2
+; R2 = padrão de bits de habilitação das UARTs
+; Destrói: R0 e R1
+UART_enable:
+        LDR R0, =SYSCTL_BASE
+	LDR R1, [R0, #SYSCTL_RCGCUART]
+	ORR R1, R2 ; habilita UARTs selecionados
+	STR R1, [R0, #SYSCTL_RCGCUART]
+
+waitu	LDR R1, [R0, #SYSCTL_PRUART]
+	TEQ R1, R2 ; clock das UARTs habilitados?
+	BNE waitu
+
+        BX LR
+        
+; UART_config: configura a UART desejada
+; R0 = endereço base da UART desejada
+; Destrói: R1
+UART_config:
+        LDR R1, [R0, #UART_CTL]
+        BIC R1, #0x01 ; desabilita UART (bit UARTEN = 0)
+        STR R1, [R0, #UART_CTL]
+
+        ; clock = 16MHz, baud rate = 300 bps
+        MOV R1, #3333
+        STR R1, [R0, #UART_IBRD]
+        MOV R1, #22
+        STR R1, [R0, #UART_FBRD]
+        
+        ; 8 bits, 2 stop, even parity, FIFOs disabled, no interrupts
+        MOV R1, #0x6E ;01101110b 
+        STR R1, [R0, #UART_LCRH]
+        
+        ; clock source = system clock
+        MOV R1, #0x00
+        STR R1, [R0, #UART_CC]
+        
+        LDR R1, [R0, #UART_CTL]
+        ORR R1, #0x01 ; habilita UART (bit UARTEN = 1)
+        STR R1, [R0, #UART_CTL]
+
+        BX LR
+
+
+; GPIO_special: habilita funcões especiais no port de GPIO desejado
+; R0 = endereço base do port desejado
+; R1 = padrão de bits (1) a serem habilitados como funções especiais
+; Destrói: R2
+GPIO_special:
+	LDR R2, [R0, #GPIO_AFSEL]
+	ORR R2, R1 ; configura bits especiais
+	STR R2, [R0, #GPIO_AFSEL]
+
+	LDR R2, [R0, #GPIO_DEN]
+	ORR R2, R1 ; habilita função digital
+	STR R2, [R0, #GPIO_DEN]
+
+        BX LR
+
+; GPIO_select: seleciona funcões especiais no port de GPIO desejado
+; R0 = endereço base do port desejado
+; R1 = máscara de bits a serem alterados
+; R2 = padrão de bits (1) a serem selecionados como funções especiais
+; Destrói: R3
+GPIO_select:
+	LDR R3, [R0, #GPIO_PCTL]
+        BIC R3, R1
+	ORR R3, R2 ; seleciona bits especiais
+	STR R3, [R0, #GPIO_PCTL]
+
+        BX LR
+;----------
+
+; GPIO_enable: habilita clock para os ports de GPIO selecionados em R2
+; R2 = padrão de bits de habilitação dos ports
+; Destrói: R0 e R1
+GPIO_enable:
+        LDR R0, =SYSCTL_BASE
+	LDR R1, [R0, #SYSCTL_RCGCGPIO]
+	ORR R1, R2 ; habilita ports selecionados
+	STR R1, [R0, #SYSCTL_RCGCGPIO]
+
+waitg	LDR R1, [R0, #SYSCTL_PRGPIO]
+	TEQ R1, R2 ; clock dos ports habilitados?
+	BNE waitg
+
+        BX LR
+
+        
+        SECTION .rodata:CONST(2)
+        DATA
+   
+REPLY                   DC8     "Sistemas Microcontrolados\r\n" 
+        
+        END
